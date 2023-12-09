8ff424f3c5c2ca76a1c68b170beda27ce2095588
Darwin Jesus Neira Carrasco <djnc670@hotmail.com>
2023-12-09 10:26:38 -0500

    resolucion de problema de navegacion entre fragments

--- app/src/main/java/com/example/idnp_sunshield/MainActivity.java
+++ app/src/main/java/com/example/idnp_sunshield/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
     private void replaceFragment(Fragment fragment){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.replace(R.id.frame_container, fragment);
         fragmentTransaction.commit();
     }
     /*
