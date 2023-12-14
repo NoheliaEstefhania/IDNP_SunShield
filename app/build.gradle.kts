ee2d0233f827fa41dd17200c114402a78e1298bd
netaap <ntacca@unsa.edu.pe>
2023-12-13 19:39:25 -0500

    Implementación del fragment Health, las imágenes e información se muestran correctamente, sin embargo la funcionalidad no es la esperada aún

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 
 dependencies {
 
+    implementation ("androidx.recyclerview:recyclerview:1.2.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -46,6 +47,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
--- app/src/main/java/com/example/idnp_sunshield/Health.java
+++ app/src/main/java/com/example/idnp_sunshield/Health.java
@@ -1,64 +1,70 @@
 package com.example.idnp_sunshield;
 
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Health#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Health extends Fragment {
+    String[] names = {"disease01", "disease02", "disease03", "disease04", "disease05"};
+    String[] descriptions = {"disease01", "disease02", "disease03", "disease04", "disease05"};
+    int[] photos = {R.drawable.img_disease01, R.drawable.img_disease03, R.drawable.img_disease01, R.drawable.img_disease03,R.drawable.img_disease01};
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Health() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Health.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Health newInstance(String param1, String param2) {
-        Health fragment = new Health();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    RecyclerView rv1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_health, container, false);
+        View view = inflater.inflate(R.layout.fragment_health, container, false);
+
+        rv1 = view.findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        rv1.setLayoutManager(linearLayoutManager);
+        rv1.setAdapter(new AdapterDisease());
+
+        return view;
+    }
+
+    private class AdapterDisease extends RecyclerView.Adapter<AdapterDisease.AdapterDiseaseHolder> {
+        @NonNull
+        @Override
+        public AdapterDiseaseHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item, parent, false);
+            return new AdapterDiseaseHolder(view);
+        }
+
+        @Override
+        public int getItemCount() {
+            return names.length;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull AdapterDiseaseHolder holder, int position) {
+            holder.print(position);
+        }
+
+        private class AdapterDiseaseHolder extends RecyclerView.ViewHolder{
+            TextView tv1, tv2;
+            ImageView iv1;
+
+            public AdapterDiseaseHolder(@NonNull View itemView) {
+                super(itemView);
+                iv1 = itemView.findViewById(R.id.imageView_disease);
+                tv1 = itemView.findViewById(R.id.textView_name);
+                tv2 = itemView.findViewById(R.id.textView_description);
+            }
+
+            public void print(int position) {
+                iv1.setImageResource(photos[position]);
+                tv1.setText(names[position]);
+                tv2.setText(descriptions[position]);
+            }
+        }
     }
 }
