e29f01286c7c05cdbcb11bb6fcc40da4e5bafcfc
Jackson Merma <jmermap@unsa.edu.pe>
2023-12-14 20:54:35 -0500

    Definición de la entidad Illness

---
+++ app/src/main/java/com/example/idnp_sunshield/Entity/Illness.java
@@ -0,0 +1,36 @@
+package com.example.idnp_sunshield.Entity;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "Illness")
+public class Illness {
+    @ColumnInfo(name = "illness_id")
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "title")
+    private String title;
+
+    @ColumnInfo(name = "description")
+    private String description;
+
+    @ColumnInfo(name = "image", typeAffinity = ColumnInfo.BLOB)
+    private byte[] image;
+    public Illness(String title, String description, byte[] image){
+        this.title = title;
+        this.description = description;
+        this.image = image;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+    public String getDescription(){
+        return this.description;
+    }
+    public byte[] getImage(){
+        return this.image;
+    }
+}
