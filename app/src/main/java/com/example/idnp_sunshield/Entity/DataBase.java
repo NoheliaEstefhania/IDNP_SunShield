d44ecae41993fa36c1f1e70cff4922c76b3dc2bb
Jackson Merma <jmermap@unsa.edu.pe>
2023-12-14 21:21:54 -0500

    Creación de la BD para ambas entidades

---
+++ app/src/main/java/com/example/idnp_sunshield/Entity/DataBase.java
@@ -0,0 +1,10 @@
+package com.example.idnp_sunshield.Entity;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Location.class, Illness.class}, version = 1)
+public abstract class DataBase extends RoomDatabase {
+    public abstract LocationDAO getLocationDAO();
+    public abstract IllnessDAO getIllnessDAO();
+}
