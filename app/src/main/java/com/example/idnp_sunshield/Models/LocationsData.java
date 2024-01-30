824f8dd21b56bfb02d855086beaaca9348f52c8e
Darwin Jesus Neira Carrasco <djnc670@hotmail.com>
2024-01-30 13:18:33 -0500

    Creacion de la clase LocationData, elimiacion de clase lon y lat

--- app/src/main/java/com/example/idnp_sunshield/Models/LocationsData.java
+++ app/src/main/java/com/example/idnp_sunshield/Models/LocationsData.java
@@ -1,2 +1,22 @@
-package com.example.idnp_sunshield.Models;public class LocationsData {
+package com.example.idnp_sunshield.Models;
+
+import java.util.List;
+
+public class LocationsData {
+    double lon;
+    double lat;
+    public double getLon() {
+        return lon;
+    }
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
 }
