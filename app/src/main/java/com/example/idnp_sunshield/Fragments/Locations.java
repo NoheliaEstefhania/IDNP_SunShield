566c48fe337eb9a2740432ad76372673b3584c08
Darwin Jesus Neira Carrasco <djnc670@hotmail.com>
2024-01-30 13:21:07 -0500

    Implemtacin de guardado de informacion en la base de datos de location

--- app/src/main/java/com/example/idnp_sunshield/Fragments/Locations.java
+++ app/src/main/java/com/example/idnp_sunshield/Fragments/Locations.java
@@ -1,30 +1,21 @@
 package com.example.idnp_sunshield.Fragments;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.room.Room;
 
 import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
-import android.widget.TextView;
 
 import com.example.idnp_sunshield.Entity.DataBase;
-import com.example.idnp_sunshield.Entity.Illness;
 import com.example.idnp_sunshield.Entity.Location;
 import com.example.idnp_sunshield.Interfaces.InterfaceApi;
-import com.example.idnp_sunshield.Models.lat;
-import com.example.idnp_sunshield.Models.lon;
-import com.example.idnp_sunshield.R;
+import com.example.idnp_sunshield.Models.LocationsData;
 import com.example.idnp_sunshield.databinding.FragmentLocationBinding;
 
-import java.io.ByteArrayOutputStream;
 import java.util.List;
 
 import retrofit2.Call;
@@ -44,8 +35,6 @@ public class Locations extends Fragment {
 
     double latitud;
     double longitud;
-    boolean fetchLati = false;
-    boolean fetchLont = false;
 
     String countryLocation;
 
@@ -86,7 +75,6 @@ public class Locations extends Fragment {
         // Inflate the layout for this fragment using the binding object
         binding = FragmentLocationBinding.inflate(inflater, container, false);
         // Fetch weather data and update UI components
-
         binding.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -98,27 +86,23 @@ public class Locations extends Fragment {
                 }
                 System.out.println("countryLocation: " + countryLocation);
                 fetchWeather(countryLocation);
-                fetchWeather2(countryLocation);
             }
         });
-/*        if(TextUtils.isEmpty(binding.locationText.getText().toString())){
-            fetchWeather("London");
-            fetchWeather2("London");
-        }*/
+        if(TextUtils.isEmpty(binding.locationText.getText().toString())){
+            countryLocation = "London";
+            fetchWeather(countryLocation);
+        }
 
         return binding.getRoot();
     }
 
-    private void data(){
-        // List to store illnesses retrieved from the database
-        List<Location> locationsList;
-
+/*    private void data(){
         // Create or open the Room database
         DataBase dataBase = Room.databaseBuilder(
                 getActivity().getApplicationContext(),
                 DataBase.class,
                 "dbPruebas"
-        ).allowMainThreadQueries().build();
+        ).addMigrations(DataBase.MIGRATION_1_2).allowMainThreadQueries().build();
 
         // Dummy data: byte array representing an image (replace with actual image data)
 
@@ -134,21 +118,11 @@ public class Locations extends Fragment {
         System.out.println("valor setter de laitud en data : " + getLatitud());
         System.out.println("valor setter de longitud en data : " + getLongitud());
 
-        // Add an illness with the image to the database
-        dataBase.getLocationDAO().addLocation(new Location(a, b));
-
         // Retrieve all illnesses from the database
+        // List to store illnesses retrieved from the database
+        List<Location> locationsList;
         locationsList = dataBase.getLocationDAO().getAllLocations();
 
-        System.out.println("valores de locations list: " + locationsList.toString());
-        // Display the title of the first illness in the TextView2
-        System.out.println("Cantidad de valores de locationList antes : " + locationsList.toArray().length);
-
-        for (int i = 0; i < locationsList.toArray().length; i++) {
-            System.out.println("valores de locations list: " + locationsList.get(i).getLongitude());
-            System.out.println("valores de locations list: " + locationsList.get(i).getLatitude());
-
-        }
         //System.out.println(dataBase.getLocationDAO().getLocation(locationsList.get(locationsList.toArray().length -1).getId()).getClass().getName());
         for (int i = 0; i < locationsList.toArray().length; i++) {
             System.out.println("Buscanodo si ya esta registrado el valor");
@@ -162,114 +136,112 @@ public class Locations extends Fragment {
             else
                 dataBase.getLocationDAO().addLocation(new Location(a, b));
         }
+        System.out.println("valores de locations list: " + locationsList.toString());
+        // Display the title of the first illness in the TextView2
+        System.out.println("Cantidad de valores de locationList antes : " + locationsList.toArray().length);
+
+        for (int i = 0; i < locationsList.toArray().length; i++) {
+            System.out.println("valores de locations list: " + locationsList.get(i).getLongitude());
+            System.out.println("valores de locations list: " + locationsList.get(i).getLatitude());
+
+        }
+
         binding.showCountry.setText(locationsList.get(0).getLatitude() + locationsList.get(0).getLongitude() + "");
         for (int i = 0; i < locationsList.toArray().length; i++) {
             dataBase.getLocationDAO().deleteLocation(locationsList.get(i));
         }
         System.out.println("Cantidad de valores de locationList despeus: " + locationsList.toArray().length);
 
+    }*/
+
+    private void data(String countryLocation) {
+        DataBase dataBase = Room.databaseBuilder(
+                getActivity().getApplicationContext(),
+                DataBase.class,
+                "dbPruebas"
+        ).allowMainThreadQueries().build();
+
+        double latitud = getLatitud();
+        double longitud = getLongitud();
+
+        System.out.println("Valores de latitud y longitud: " + latitud + ", " + longitud);
+        List<Location> locationsList;
+        locationsList = dataBase.getLocationDAO().getAllLocations();
+
+        boolean locationAlreadyRegistered = false;
+
+        for (Location location : locationsList) {
+            System.out.println("Impresion de valor de location.getLatitude() : "+ location.getLatitude());
+            System.out.println("Impresion de valor de latitud : "+ latitud);
+            System.out.println("Impresion de valor de location.getLongitude() : "+ location.getLongitude());
+            System.out.println("Impresion de valor de longitud : "+ longitud);
+
+            if (location.getLatitude() == latitud && location.getLongitude() == longitud) {
+                locationAlreadyRegistered = true;
+                System.out.println("Ya está registrada");
+                break;
+            }
+        }
+
+        if (!locationAlreadyRegistered) {
+            System.out.println("entre al registro");
+            dataBase.getLocationDAO().addLocation(new Location(longitud, latitud, countryLocation));
+
+        }
+
+        locationsList = dataBase.getLocationDAO().getAllLocations();
+
+        System.out.println("Cantidad de valores de locationsList antes: " + locationsList.size());
+
+        for (Location location : locationsList) {
+            System.out.println("Valores de locationsList: Country: "+  location.getTitle() + " Latitud=" + location.getLatitude() +
+                    ", Longitud=" + location.getLongitude());
+        }
+
+        //binding.showCountry.setText(!locationsList.isEmpty() ? (locationsList.get(0).getLatitude() + locationsList.get(0).getLongitude() + "") : "");
+        binding.showCountry.setText(locationsList.get(0).getLatitude() + " "+ locationsList.get(0).getLongitude() + " ");
+
+        /*for (int i = 0; i < locationsList.toArray().length; i++) {
+            dataBase.getLocationDAO().deleteLocation(locationsList.get(i));
+        }*/
+        System.out.println("Cantidad de valores de locationsList después: " + locationsList.size());
     }
+
     public void fetchWeather(String countryLocation) {
         // Create a Retrofit instance with the base URL and GsonConverterFactory
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.openweathermap.org/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-
-
         // Create an instance of the InterfaceApi using the Retrofit instance
         InterfaceApi interfaceApi = retrofit.create(InterfaceApi.class);
         System.out.println("Location Messaje");
         // Make an asynchronous call to get UV data from OpenWeatherMap API
-        //Call<List<CountryData>> call = interfaceApi.getCountry( "London",1, "c71298943776351e81c2f4e84456a36d");
-        Call<List<lat>> call = interfaceApi.getLat( countryLocation,1, "c71298943776351e81c2f4e84456a36d");
+        Call<List<LocationsData>> call = interfaceApi.getLocationData( countryLocation,1, "c71298943776351e81c2f4e84456a36d");
 
         System.out.println("CALL :" + call.request().url());
-        call.enqueue(new Callback<List<lat>>() {
+        call.enqueue(new Callback<List<LocationsData>>() {
             @Override
-            public void onResponse(Call<List<lat>> call, Response<List<lat>> response) {
+            public void onResponse(Call<List<LocationsData>> call, Response<List<LocationsData>> response) {
+
                 if (response.isSuccessful()) {
-                    // Retrieve UVData from the response
-                    //country country = response.body();
-                    List<lat> lati = response.body();
+                    List<LocationsData> lodata = response.body();
+                    double lon = lodata.get(0).getLon();
+                    double lati = lodata.get(0).getLat();
                     System.out.println("response.body contenido: " + response.body());
                     System.out.println("Location Messaje on response");
-                    System.out.println("Lat[0] valor : " + lati.get(0));                    System.out.println("Lat[0] valor : " + lati.get(0));
-                    System.out.println("Lat[0] valor : " + lati.get(0).getLat());
-
-
-                    setLatitud(lati.get(0).getLat());
-                    System.out.println("valor setter de laitud: " + getLatitud());
-                    fetchLati = true;
-                    tryExecute();
-                    // Extract main and current data from UVData
-                    //main to = country.getMain();
-                    // Update UI components with UV index value
-                    //binding.showCountry.setText("conexion");
-                    //binding.showCountry.setText(String.valueOf(tcLan));
-
+                    setLongitud(lon);
+                    setLatitud(lati);
+                    System.out.println("valor setter de longitud: " + lati + " " + lon);
+                    data(countryLocation);
                 }
             }
-
             @Override
-            public void onFailure(Call<List<lat>> call, Throwable t) {
+            public void onFailure(Call<List<LocationsData>> call, Throwable t) {
                 binding.showCountry.setText("no answer");
+                System.out.println("Error: " + t.getMessage());
+                t.printStackTrace();
             }
         });
-    }
-
-    public void fetchWeather2(String countryLocation) {
-        // Create a Retrofit instance with the base URL and GsonConverterFactory
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.openweathermap.org/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        // Create an instance of the InterfaceApi using the Retrofit instance
-        InterfaceApi interfaceApi = retrofit.create(InterfaceApi.class);
-        System.out.println("Location Messaje");
-        // Make an asynchronous call to get UV data from OpenWeatherMap API
-        //Call<List<CountryData>> call = interfaceApi.getCountry( "London",1, "c71298943776351e81c2f4e84456a36d");
-        Call<List<lon>> call = interfaceApi.getLon( countryLocation,1, "c71298943776351e81c2f4e84456a36d");
-
-        System.out.println("CALL :" + call.request().url());
-        call.enqueue(new Callback<List<lon>>() {
-            @Override
-            public void onResponse(Call<List<lon>> call, Response<List<lon>> response) {
-                if (response.isSuccessful()) {
-                    // Retrieve UVData from the response
-                    //country country = response.body();
-                    List<lon> loni = response.body();
-                    System.out.println("response.body contenido: " + response.body());
-                    System.out.println("Location Messaje on response");
-                    System.out.println("Lat[0] valor : " + loni.get(0));
-                    System.out.println("Lat[0] valor : " + loni.get(0));
-                    System.out.println("Lat[0] valor : " + loni.get(0).getLon());
-
-                    setLongitud(loni.get(0).getLon());
-                    System.out.println("valor setter de longitud: " + getLongitud());
-
-                    fetchLont = true;
-                    tryExecute();
-                    // Extract main and current data from UVData
-                    //main to = country.getMain();
-                    // Update UI components with UV index value
-                    //binding.showCountry.setText("conexion");
-                    //binding.showCountry.setText(String.valueOf(tcLan));
-
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<lon>> call, Throwable t) {
-                binding.showCountry.setText("no answer");
-            }
-        });
-
-    }
-
-    private void tryExecute(){
-        if(fetchLati & fetchLont)
-            data();
     }
 }
