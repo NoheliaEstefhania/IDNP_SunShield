a8ec38061c18cbce33f039b646e6206ad350342a
Darwin Jesus Neira Carrasco <djnc670@hotmail.com>
2024-02-01 20:42:30 -0500

    Aplicando el patron Singleton para para las variable de Location

--- app/src/main/java/com/example/idnp_sunshield/Singleton/LocationSingleton.java
+++ app/src/main/java/com/example/idnp_sunshield/Singleton/LocationSingleton.java
@@ -1,4 +1,30 @@
 package com.example.idnp_sunshield.Singleton;
 
+import com.example.idnp_sunshield.Entity.Location;
+
 public class LocationSingleton {
+    private static LocationSingleton instance;
+
+    // Atributos de la ubicación que desees almacenar
+    private Location location;
+
+    private LocationSingleton() {
+        // Constructor privado para evitar la creación de instancias directas
+    }
+
+    public static LocationSingleton getInstance() {
+        if (instance == null) {
+            instance = new LocationSingleton();
+        }
+        return instance;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
 }
+
