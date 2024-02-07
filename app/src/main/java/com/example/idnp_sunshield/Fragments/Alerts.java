3864f3342c3293aba8ef76fbbe6c64ff67c8bd77
Darwin Jesus Neira Carrasco <djnc670@hotmail.com>
2024-02-06 19:29:48 -0500

    comentarios de metodos

--- app/src/main/java/com/example/idnp_sunshield/Fragments/Alerts.java
+++ app/src/main/java/com/example/idnp_sunshield/Fragments/Alerts.java
@@ -4,7 +4,6 @@ package com.example.idnp_sunshield.Fragments;
 import android.content.Context;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -15,15 +14,12 @@ import androidx.room.Room;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Switch;
-import android.widget.TextView;
 
 import com.example.idnp_sunshield.Adapter_ViewHolder.Alerts_Adapter_ViewHolder.AlertsAdapter;
 import com.example.idnp_sunshield.Entity.DataBase;
 
 import com.example.idnp_sunshield.Entity.Location;
 import com.example.idnp_sunshield.R;
-import com.example.idnp_sunshield.SharePreferences.LocationPreferences;
 import com.example.idnp_sunshield.databinding.FragmentAlertsBinding;
 
 import java.util.List;
@@ -33,6 +29,7 @@ public class Alerts extends Fragment {
     FragmentAlertsBinding binding;
 
     // Default constructor
+    // This constructor initializes the Alerts fragment.
     public Alerts() {
         // Required empty public constructor
     }
@@ -41,6 +38,9 @@ public class Alerts extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    // Method to create and return the view of the fragment
+    // This method inflates the layout for the Alerts fragment,
+    // sets up UI elements, and fetches data to display.
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -48,10 +48,13 @@ public class Alerts extends Fragment {
         binding = FragmentAlertsBinding.inflate(inflater, container, false);
         // Call the data method to load and display information
         // Return the root view of the fragment
+
+        // Set up onClickListener for a button to navigate to another fragment
         binding.button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 System.out.println("En el boton de alerts");
+                // Navigate to Locations fragment when the button is clicked
                 Locations locations_fragment = new Locations();
                 FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -67,14 +70,14 @@ public class Alerts extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         recyclerView.setHasFixedSize(true);
 
-
+        // Initialize database and fetch list of locations
         DataBase dataBase = Room.databaseBuilder(
                 getActivity().getApplicationContext(),
                 DataBase.class,
                 "dbPruebas"
         ).addMigrations(DataBase.MIGRATION_2_3).allowMainThreadQueries().build();
-        List<Location> locationsList;
-        locationsList = dataBase.getLocationDAO().getAllLocations();
+        List<Location> locationsList = dataBase.getLocationDAO().getAllLocations();
+        // Print values of locationsList for debugging purposes
         for (Location location : locationsList) {
             System.out.println("Valores de locationsList: Country: "+  location.getTitle() + " Latitud=" + location.getLatitude() +
                     ", Longitud=" + location.getLongitude() + ", State: " + location.getState());
@@ -87,8 +90,8 @@ public class Alerts extends Fragment {
         return binding.getRoot();
     }
 
-
-
+    // Method to get the current activity fragment
+    // This method returns the current activity fragment.
     public Fragment getCurrentActivity() {
         return this;
     }
